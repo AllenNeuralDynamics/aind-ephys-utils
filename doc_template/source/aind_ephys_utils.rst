b3b3ca5645f95b2386e60ee79be3f59a4b490664
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2022-12-12 17:26:44 -0800

    feat: inits library

---
+++ doc_template/source/aind_ephys_utils.rst
@@ -0,0 +1,21 @@
+aind\_ephys\_utils package
+==========================
+
+Submodules
+----------
+
+aind\_ephys\_utils.hello\_world\_message\_handlers module
+---------------------------------------------------------
+
+.. automodule:: aind_ephys_utils.hello_world_message_handlers
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: aind_ephys_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ doc_template/source/conf.py
@@ -0,0 +1,48 @@
+"""Configuration file for the Sphinx documentation builder."""
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path Setup --------------------------------------------------------------
+import os
+import sys
+
+import aind_ephys_utils
+
+sys.path.insert(0, os.path.abspath("../../src"))
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "aind-ephys-utils"
+copyright = "2022, Allen Institute of Neural Dynamics"
+author = "Allen Institute of Neural Dynamics"
+release = aind_ephys_utils.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_favicon = "_static/favicon.ico"
+html_theme_options = {
+    "light_logo": "light-logo.svg",
+    "dark_logo": "dark-logo.svg",
+}
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = False
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = False
